216f73442c79c924b6bd4efdc909ad8e1512c06f
coinop-logan <coinop.logan@gmail.com>
2022-03-08 12:44:29 +0200

    Simply Python script to extract address from signed message

---
+++ py/signed_msg_to_address.py
@@ -0,0 +1,14 @@
+from web3 import Web3
+from hexbytes import HexBytes
+from eth_account.messages import encode_defunct
+
+def signed_msg_to_address(msgStr, sigStr):
+    w3 = Web3()
+
+    encodedMsg = encode_defunct(text=msgStr)
+
+    if sigStr[:2] != "0x":
+        sigStr = "0x" + sigStr
+    encodedSig = HexBytes(sigStr)
+
+    return w3.eth.account.recover_message(encodedMsg, signature=encodedSig)
