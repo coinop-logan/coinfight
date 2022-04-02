7a3681e25c1172025d980ca038b365e2e1af7a53
coinop-logan <coinop.logan@gmail.com>
2022-04-02 14:06:19 +0200

    Include local demo in dist

--- package-assets/client/package.sh
+++ package-assets/client/package.sh
@@ -6,6 +6,7 @@ mkdir coinfight-client-linux
 cp install.sh coinfight-client-linux/
 cp ../../bin/Andale_Mono.ttf coinfight-client-linux/
 cp ../../bin/client coinfight-client-linux/coinfight
+cp ../../bin/coinfight_local coinfight-client-linux/local_demo
 
 zip -r coinfight-client-linux.zip coinfight-client-linux
 rm -rf coinfight-client-linux
