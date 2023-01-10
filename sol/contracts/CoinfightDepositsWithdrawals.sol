2cc9f4ee9b3f5b2d84fd1cd20239033707788e82
coinop-logan <coinop.logan@gmail.com>
2023-01-09 15:03:06 -0900

    Update contract to handle ERC20

--- sol/contracts/CoinfightDepositsWithdrawals.sol
+++ sol/contracts/CoinfightDepositsWithdrawals.sol
@@ -1,51 +1,62 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CoinfightDepositsWithdrawals is Ownable {
     // owner is the game server
+
+    IERC20 public token;
+
+    constructor(IERC20 _token)
+    {
+        token = _token;
+    }
 
     // deposit is called by user (presumably through a dapp)
     // event is emitted for the server to track, and the amount is forwarded to the server
     event DepositMade(address payable forAccount, uint amount);
 
-    function deposit(address payable forAccount)
-        payable
+    function pullDeposit(uint amount)
+        internal
+    {
+        require(token.transferFrom(msg.sender, address(this), amount), "Token transfer failed.");
+    }
+
+    function deposit(address payable forAccount, uint amount)
         external
     {
-        require(msg.value > 0, "You're trying to deposit with a value of 0...");
+        require(amount > 0, "You're trying to deposit with a value of 0...");
 
-        emit DepositMade(forAccount, msg.value);
+        pullDeposit(amount);
 
-        payable(owner()).transfer(msg.value);
+        emit DepositMade(forAccount, amount);
     }
 
     event HoneypotAdded(uint amount);
 
-    function addHoneypot()
-        payable
+    function addHoneypot(uint amount)
         external
     {
-        require(msg.value > 0, "You're trying to create a honeypot with a value of 0...");
+        require(amount > 0, "You're trying to create a honeypot with a value of 0...");
 
-        emit HoneypotAdded(msg.value);
+        pullDeposit(amount);
 
-        payable(owner()).transfer(msg.value);
+        emit HoneypotAdded(amount);
     }
 
     // withdraw is called by server
     // event is emitted and amount is sent to player
     event WithdrawalMade(address payable toAccount, uint amount);
 
-    function withdraw(address payable toAccount)
-        payable
+    function withdraw(address payable toAccount, uint amount)
         external
         onlyOwner
     {
-        require (msg.value > 0, "You're trying to process a withdrawal value of 0...");
+        require (amount > 0, "You're trying to process a withdrawal value of 0...");
 
-        emit WithdrawalMade(toAccount, msg.value);
+        require(token.transfer(toAccount, amount), "Token transfer failed.");
 
-        toAccount.transfer(msg.value);
+        emit WithdrawalMade(toAccount, amount);
     }
 }
